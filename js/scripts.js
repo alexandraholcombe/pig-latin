f05741979740f1879269c91b66d12feeb10f0d28
Alexandra Holcombe and Renee Mei <renee@>
2017-01-18 15:09:19 -0800

    add scripts to move the first two consonants to the end and add ay if first two are consonants add script to clear vowelArray

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,17 @@ var vowels = ["a", "e", "i", "o", "u"];
 var vowelsVal = [];
 var vvArray = [];
 
+
+var concatAy = function(input, index) {
+  consonant = input.substring(0,index);
+  rest = input.substring(index,input.length);
+  return rest + consonant + "ay"
+}
+
 var vowelChecker = function(input) {
+  vowelsVal = [];
   inputArray = input.split("");
-  for (inputIndex = 0; inputIndex < userInput.length; inputIndex++){
+  for (inputIndex = 0; inputIndex < input.length; inputIndex++){
     vowels.forEach(function(vowel) {
       if (inputArray[inputIndex] === vowel) {
         vowelsVal.push(1);
@@ -22,24 +30,16 @@ var vowelChecker = function(input) {
 
 var translate = function(input) {
   var piglatin = 0;
+  vvArray = vowelChecker(input);
   if (parseFloat(input)) {
     return input;
-  } else {
-      vvArray = vowelChecker(input);
-      if (!vvArray[0]){
-        consonant = input.substring(0,1);
-        rest = input.substring(1,input.length);
-        return rest + consonant + "ay";
-      }
+  } else if(!(vvArray[0]) && !(vvArray[1])) {
+      return concatAy(input, 2);
+  } else if(!vvArray[0]){
+      return concatAy(input, 1);
   }
-}
-    // if(!piglatin){
-    //
-    //   consonant = input.substring(0,1);
-    //   rest = input.substring(1,input.length);
-    //   return rest + consonant + "ay";
-    // }
 
+}
 
 
 //user interface logic
@@ -54,3 +54,14 @@ $(document).ready(function() {
 
 
 // testing
+
+// for (ind = 0; ind <= 3; ind++){
+//   if (!vvArray[ind]){
+//     consonant = input.substring(0,ind+1);
+//     rest = input.substring(ind+1,input.length);
+//   }
+// }
+//     return rest + consonant + "ay";
+// }else{
+//   return input + "ay"
+// }
