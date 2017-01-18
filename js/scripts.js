9e10554523eb4add4104fe320e8c2b64028ae908
Alexandra Holcombe and Renee Mei <renee@>
2017-01-18 10:34:09 -0800

    add script to add ay to the end of a single letter word starting with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //business logic
 var userInput = 0;
-
+var vowels = ["a", "e", "i", "o", "u"];
 var translate = function(input) {
-  return input;
+  var inputControl = 0;
+  for(index = 0; index <= 4; index++){
+    console.log(index);
+    if(input===vowels[index]){
+      alert("test");
+      inputControl = 1;
+      return input.concat("ay");
+    };
+  }
+  if (!inputControl){
+    return input;
+  }
 }
+
 
 //user interface logic
 $(document).ready(function() {
   $("form").submit(function (event) {
-      event.preventDefault();
-      userInput = $("#user-input").val();
-      var result = translate(userInput);
-      $("#result").text(result);
+    event.preventDefault();
+    userInput = $("#user-input").val();
+    var result = translate(userInput);
+    $("#result").text(result);
   });
 });
